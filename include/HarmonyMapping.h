fefc28e85b119c6a8cc5cc6b4ae138a07545286e
韩兴华 <Vlairrrrr@outlook.com>
2021-06-02 13:44:04 +0800

    Revert "Update Comments"
    This reverts commit 4b6420371daf0c115ab466d61d0b108fe694dcc4.

--- include/HarmonyMapping.h
+++ include/HarmonyMapping.h
@@ -22,15 +22,14 @@ namespace MeshLib
 	template<typename M,typename V>
 	class HarmonyMapping
 	{
-		map<int,int>* pVMap = NULL; //矩阵下标和VertexID构成的映射
-		M* p_mesh;//网格
-		void SetTransfer(M* mesh);//初始化矩阵下标和VertexID的映射
-		CHalfEdge* FindNextBoundaryHE(CHalfEdge* he);//找到边界半边所指向的下一个半边
+		map<int,int>* pVMap = NULL;
+		M* p_mesh;
+		void SetTransfer(M* mesh);
 	public:
-		void Set_InsideUV(M* mesh);//边界映射设定好的前提下，设置内部的映射
-		void SetUV_CircleBorder(M* mesh);//设定边界映射为圆形的简单映射
-		double GetVertexWeight(V* v1, V* v2,M* mesh);//获得两点间的权重
-		void StartMapping();//调和映照启动函数
+		void Set_InsideUV(M* mesh);
+		void SetUV_CircleBorder(M* mesh);
+		double GetVertexWeight(V* v1, V* v2,M* mesh);
+		void StartMapping();
 		HarmonyMapping(M* pMesh);
 		~HarmonyMapping()
 		{
@@ -62,7 +61,7 @@ namespace MeshLib
 			!vertex_iterator.end();
 			vertex_iterator++)
 		{
-			VMAP[vertex_iterator.value()->id()]=i++; //根据网格内点遍历顺序构建map
+			VMAP[vertex_iterator.value()->id()]=i++;
 		}
 	}
 
@@ -70,20 +69,17 @@ namespace MeshLib
 	template<typename M,typename V>
 	void HarmonyMapping<M,V>::Set_InsideUV(M* mesh)
 	{
-		const int numVertices = mesh->numVertices()+1; //矩阵总维数
-		Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> matrix_x = Eigen::MatrixXd::Zero(numVertices, numVertices); //映射u坐标方程系数矩阵
-		Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> matrix_y = Eigen::MatrixXd::Zero(numVertices, numVertices); //映射v坐标方程系数矩阵
-		Eigen::Matrix<double, Eigen::Dynamic, 1> b_x = Eigen::MatrixXd::Zero(numVertices, 1); //映射u坐标方程常数向量
-		Eigen::Matrix<double, Eigen::Dynamic, 1> b_y = Eigen::MatrixXd::Zero(numVertices, 1); //映射v坐标方程常数向量
-		Eigen::Matrix<double, Eigen::Dynamic, 1> res_x = Eigen::MatrixXd::Zero(numVertices, 1); //映射u坐标结果
-		Eigen::Matrix<double, Eigen::Dynamic, 1> res_y = Eigen::MatrixXd::Zero(numVertices, 1); //映射v坐标结果
-
-		//对每个点构建一条方程
+		const int numVertices = mesh->numVertices()+1;
+		Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> matrix_x = Eigen::MatrixXd::Zero(numVertices, numVertices);
+		Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> matrix_y = Eigen::MatrixXd::Zero(numVertices, numVertices);
+		Eigen::Matrix<double, Eigen::Dynamic, 1> b_x = Eigen::MatrixXd::Zero(numVertices, 1);
+		Eigen::Matrix<double, Eigen::Dynamic, 1> b_y = Eigen::MatrixXd::Zero(numVertices, 1);
+		Eigen::Matrix<double, Eigen::Dynamic, 1> res_x = Eigen::MatrixXd::Zero(numVertices, 1);
+		Eigen::Matrix<double, Eigen::Dynamic, 1> res_y = Eigen::MatrixXd::Zero(numVertices, 1);
 		for(MeshVertexIterator<CToolVertex, CToolEdge, CToolFace, CToolHalfEdge> vertex_iterator(mesh);
 			!vertex_iterator.end();
 			++vertex_iterator)
 		{
-			//如果v是边界点，则构建方程 v = v.uv
 			if (vertex_iterator.value()->boundary())
 			{
 				b_x(VMAP[vertex_iterator.value()->id()]) = vertex_iterator.value()->uv()[0];
@@ -93,23 +89,16 @@ namespace MeshLib
 				matrix_y(VMAP[vertex_iterator.value()->id()], VMAP[vertex_iterator.value()->id()]) = 1;
 				continue;
 			}
-
-			//如果是内部点i,且点i周围点为j，则构建方程 Σ(Vj.weight*Vj)-Vi*Σ(weight*Vj) = 0
 			CToolVertex* vertex = vertex_iterator.value();
 			double totalWeight = 0.0;
-
-			//求出Σ(weight*Vj)
 			for (VertexVertexIterator<CToolVertex, CToolEdge, CToolFace, CToolHalfEdge> vertex_iter(vertex);
 				!vertex_iter.end();
 				vertex_iter++)
 			{
 				totalWeight += GetVertexWeight(vertex_iter.value(), vertex,mesh);
 			}
-
-			//Vi系数-Σ(weight*Vj)
 			matrix_x(VMAP[vertex_iterator.value()->id()], VMAP[vertex_iterator.value()->id()]) = -totalWeight;
 			matrix_y(VMAP[vertex_iterator.value()->id()], VMAP[vertex_iterator.value()->id()]) = -totalWeight;
-			//Vj系数Vj.weight
 			for (VertexVertexIterator<CToolVertex, CToolEdge, CToolFace, CToolHalfEdge> vertex_iter(vertex);
 				!vertex_iter.end();
 				vertex_iter++)
@@ -118,18 +107,15 @@ namespace MeshLib
 				matrix_y(VMAP[vertex->id()], VMAP[vertex_iter.value()->id()]) = GetVertexWeight(vertex_iter.value(),vertex, mesh);
 			}
 		}
-
-		//使用QR分解求方程组的解
 		res_x = matrix_x.colPivHouseholderQr().solve(b_x);
 		res_y = matrix_y.colPivHouseholderQr().solve(b_y);
-
-		//赋值UV坐标
 		for (MeshVertexIterator<CToolVertex, CToolEdge, CToolFace, CToolHalfEdge> vertex_iterator(mesh);
 			!vertex_iterator.end();
 			++vertex_iterator)
 		{
 			vertex_iterator.value()->uv()[0] = res_x[VMAP[vertex_iterator.value()->id()]];
 			vertex_iterator.value()->uv()[1] = res_y[VMAP[vertex_iterator.value()->id()]];
+			
 		}
 	}
 
@@ -138,8 +124,7 @@ namespace MeshLib
 	{
 		double res = 0.0;
 		CHalfEdge* half_edge = nullptr;
-
-		//求出两点之间的半边
+		
 		for (VertexOutHalfedgeIterator<CToolVertex, CToolEdge, CToolFace, CToolHalfEdge> 
 			half_edge_iter(mesh,v1);
 			!half_edge_iter.end();
@@ -151,19 +136,13 @@ namespace MeshLib
 				half_edge = half_edge_iter.value();
 			}
 		}
-
-		//求出三角形的第三个点
 		CVertex* v3 = half_edge->he_next()->vertex();
 		res+= (v1->point() - v3->point()) * (v2->point() - v3->point())
 			/ ((v1->point() - v3->point()) ^ (v2->point() - v3->point())).norm();
-
-		//如果是边界点，则权重只有一边的三角形
 		if (v1->boundary() && v2->boundary())
 		{
 			return res;
 		}
-
-		//指向另外的三角形
 		half_edge = half_edge->he_sym();
 		CVertex* v4 = half_edge->he_next()->vertex();
 		res+= (v1->point() - v4->point()) * (v2->point() - v4->point())
@@ -172,23 +151,10 @@ namespace MeshLib
 	}
 
 	template<typename M,typename V>
-	CHalfEdge* HarmonyMapping<M,V>::FindNextBoundaryHE(CHalfEdge* he)
-	{
-		he = he->he_next();
-		while (!he->edge()->boundary())
-		{
-			he = he->he_sym()->he_next();
-		}
-		return he;
-	}
-
-	template<typename M,typename V>
 	void HarmonyMapping<M,V>::SetUV_CircleBorder(M* mesh)
 	{
-		int boundarySum = 0;//计算边界边数量
+		int boundarySum = 0;
 		CHalfEdge* initHE = NULL;
-
-		//找到第一个位于边界的半边
 		for(MeshHalfEdgeIterator<CToolVertex, CToolEdge, CToolFace, CToolHalfEdge> he_iterator(mesh);
 			!he_iterator.end();
 			++he_iterator)
@@ -200,8 +166,6 @@ namespace MeshLib
 			}
 		}
 		CHalfEdge* half_edge = initHE;
-
-		//计算半边总数
 		CHalfEdge* half_iter = FindNextBoundaryHE(half_edge);
 		for (;half_iter!=half_edge;half_iter = FindNextBoundaryHE(half_iter))
 		{
@@ -209,8 +173,6 @@ namespace MeshLib
 		}
 		half_iter = FindNextBoundaryHE(half_iter);
 		boundarySum++;
-		
-		//边界半边长度数组
 		auto boundaryNorm = new double[boundarySum];
 		for (int i = 0; i<boundarySum;i++)
 		{
@@ -221,8 +183,6 @@ namespace MeshLib
 			}
 			half_iter = FindNextBoundaryHE(half_iter);
 		}
-
-		//计算边界点映射uv坐标
 		for (int i = 0; i < boundarySum; i++)
 		{
 			half_iter->vertex()->uv() = CPoint2(
