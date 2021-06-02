4107fc3d4d429f3b03644a5e309483e5870d055c
韩兴华 <Vlairrrrr@outlook.com>
2021-06-02 14:16:59 +0800

    OpenCV Lib and Exec Added

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,31 @@
 
 #include"Tool.h"
 #include"ToolMesh.h"
+#include <opencv2/opencv.hpp>
+
+#define PICTURE_NAME "blocks.png"
 
 
 using namespace std;
 using namespace MeshLib;
+using namespace cv;
 
+string GetExecPath(string raw_argv) //获取可执行文件目录
+{
+	string res;
+	for (int i = raw_argv.length()-1; i > 0; --i)
+	{
+		if(raw_argv[i]!='\\')
+		{
+			res = raw_argv.substr(0,i);
+		}
+		else
+		{
+			return res;
+		}
+	}
+	return res;
+}
 
 
 void main(int argc, char** argv)
@@ -19,6 +39,10 @@ void main(int argc, char** argv)
 		exit(EXIT_SUCCESS);
 	}
 	mesh.read_m(argv[1]);
+	cout<<GetExecPath(argv[0]);
+	Mat test = imread(GetExecPath(argv[0])+PICTURE_NAME); //载入图像到Mat
+    imshow("test", test);
+    waitKey(0);
 	//CTool<CTMesh,CToolVertex> tool(&mesh);
 	//Gauss-Bonet Theory
 	//tool.GaussBonetTheory();
@@ -31,8 +55,8 @@ void main(int argc, char** argv)
 	//cd \Projects\PolyCube\PolyCube\zzzbin\basic_example
 	//g3dogl.exe Out.m -texturemap check.bmp
 	//ComplexIntegral(&mesh);
-	ComplexIntegral<CTMesh,CToolVertex> intergralMesh;
-	intergralMesh.Integral(&mesh);
+	//ComplexIntegral<CTMesh,CToolVertex> intergralMesh;
+	//intergralMesh.Integral(&mesh);
 	mesh.write_m(argv[2]);
 	cout << "finished !!! press any key to continue!!!" << endl;
 	getchar();
